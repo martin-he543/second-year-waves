3463ab0524071a90a6ea9911be3a1df81089b1ce
¨martin-he543¨ <martin.he543@outlook.com>
2023-03-06 09:17:29 +0000

    Upload updated S2 data

--- 3.5_propagation_speed.py
+++ 3.5_propagation_speed.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t1, V1 = np.loadtxt("Data/Extra/SAVE51/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
-t2, V2 = np.loadtxt("Data/Extra/SAVE52/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
+t1, V1 = np.loadtxt("Data/2023.02.28/SAVE51/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
+t2, V2 = np.loadtxt("Data/2023.02.28/SAVE52/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
 t1_max = t1[np.argmax(V1)]
 t2_max = t2[np.argmax(V2)]
 print(4/(t2_max - t1_max))
@@ -11,8 +11,8 @@ plt.plot(t1, V1, label="V1")
 plt.plot(t2, V2, label="V2")
 plt.show()
 
-t1, V1 = np.loadtxt("Data/Extra/SAVE54/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
-t1, V1 = np.loadtxt("Data/Extra/SAVE55/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
+t1, V1 = np.loadtxt("Data/2023.02.28/SAVE54/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
+t1, V1 = np.loadtxt("Data/2023.02.28/SAVE55/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
 t1_max = t1[np.argmax(V1)]
 t2_max = t2[np.argmax(V2)]
 print(4/(t2_max - t1_max))
@@ -21,8 +21,8 @@ plt.plot(t1, V1, label="V1")
 plt.plot(t2, V2, label="V2")
 plt.show()
 
-t1, V1 = np.loadtxt("Data/Extra0/SAVE15/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
-t1, V1 = np.loadtxt("Data/Extra0/SAVE16/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
+t1, V1 = np.loadtxt("Data/2023.02.28/SAVE15/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
+t1, V1 = np.loadtxt("Data/2023.02.28/SAVE16/WFM.CSV", delimiter=",", skiprows=1, unpack=True)
 t1_max = t1[np.argmax(V1)]
 t2_max = t2[np.argmax(V2)]
 print(4/(t2_max - t1_max))
@@ -30,4 +30,3 @@ print(4/(t2_max - t1_max))
 plt.plot(t1, V1, label="V1")
 plt.plot(t2, V2, label="V2")
 plt.show()
-
